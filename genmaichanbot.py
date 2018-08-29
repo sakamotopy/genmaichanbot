9083f47be675aae568b57ff7c6b0cbc83c94f28f
sakamotopy <37712862+sakamotopy@users.noreply.github.com>
2018-08-29 13:50:22 +0900

    Update genmaichanbot.py

--- genmaichanbot.py
+++ genmaichanbot.py
@@ -182,18 +182,20 @@ async def on_message(message):
 
 	if message.content.startswith('test'):
 		time1 = time.time()
-		numbers1 = random.randint(0,256)
-		numbers2 = random.randint(0,512)
-		calculationnn = numbers1 ** numbers2
+		for i in range(1000000):
+		i ** 10
+#		numbers1 = random.randint(0,256)
+#		numbers2 = random.randint(0,512)
+#		calculationnn = numbers1 ** numbers2
 		time2 = time.time() - time1
 		time_result = str(time2)
-		aaaa = str(calculationnn)
-		bbbb = str(numbers1)
-		cccc = str(numbers2)
-		last_m = bbbb+" ^ "+cccc+"="+aaaa+"\n処理時間:"+time_result+"秒"
-		dddd = bbbb+" × "+cccc+"="+aaaa
-		print ("elapsed_time:{0}".format(time_result) + "[sec]")
-		await client.send_message(message.channel, last_m)
+#		aaaa = str(calculationnn)
+#		bbbb = str(numbers1)
+#		cccc = str(numbers2)
+#		last_m = bbbb+" ^ "+cccc+"="+aaaa+"\n処理時間:"+time_result+"秒"
+#		dddd = bbbb+" × "+cccc+"="+aaaa
+#		print ("elapsed_time:{0}".format(time_result) + "[sec]")
+		await client.send_message(message.channel, time_result)
 
 	if message.content.startswith('help'):
 		embed = discord.Embed(title="玄米ちゃんBOT 取扱説明書", color=0x47f558)
