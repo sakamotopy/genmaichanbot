4e85dc0dd654bb222eca4a98d295cbd3c753d03a
sakamotopy <37712862+sakamotopy@users.noreply.github.com>
2018-08-29 09:49:40 +0900

    Update genmaichanbot.py

--- genmaichanbot.py
+++ genmaichanbot.py
@@ -174,7 +174,7 @@ async def on_message(message):
 	if message.content.startswith('はあと様'):
 		await client.send_message(message.channel, 'かわいい！\n'+'https://www.youtube.com/channel/UC1CfXB_kRs3C-zaeTG3oGyg')
 
-	if message.content == "help":
+	if message.content.startswith('help'):
 		embed = discord.Embed(title="玄米ちゃんBOT 取扱説明書")
 		embed.add_field(name="help", value="使い方を表示します。")
 		embed.add_field(name="weather", value="関東周辺の天気予報を教えます。")
